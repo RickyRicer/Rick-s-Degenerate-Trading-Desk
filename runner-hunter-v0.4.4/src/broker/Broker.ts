2599777e81f6236e898ef709769ba0fbe2de3d2e
Rick Ansay <rick.ansay@gmail.com>
2026-09-02 22:37:32 -0600

    Removed old build

--- runner-hunter-v0.4.4/src/broker/Broker.ts
+++
@@ -1,8 +0,0 @@
-import type { BrokerBalance, BrokerTradeResult, Position, TokenSnapshot } from "../types/index.js";
-
-export interface Broker {
-  getBalance(): Promise<BrokerBalance>;
-  getPositions(): Promise<Position[]>;
-  buy(token: TokenSnapshot, usdAmount: number): Promise<BrokerTradeResult>;
-  sell(token: TokenSnapshot, usdAmount?: number): Promise<BrokerTradeResult>;
-}
--- runner-hunter-v0.4.4/src/version.ts
+++
@@ -1,4 +0,0 @@
-import { readFileSync } from "node:fs";
-
-const pkg = JSON.parse(readFileSync(new URL("../package.json", import.meta.url), "utf8")) as { version?: string };
-export const APP_VERSION = pkg.version ?? "unknown";
